7a9b043f5faaa1946d456b62988da9132f0adbda
Kent Fredric <kentfredric@gmail.com>
2009-05-03 09:17:26 +1200

    Module::Starter

---
+++ ELF-Extract-Sections/lib/ELF/Extract/Sections.pm
@@ -0,0 +1,107 @@
+package ELF::Extract::Sections;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+ELF::Extract::Sections - The great new ELF::Extract::Sections!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use ELF::Extract::Sections;
+
+    my $foo = ELF::Extract::Sections->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Kent Fredric, C<< <kentfredric at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-elf-extract-sections at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=ELF-Extract-Sections>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc ELF::Extract::Sections
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=ELF-Extract-Sections>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/ELF-Extract-Sections>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/ELF-Extract-Sections>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/ELF-Extract-Sections/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Kent Fredric, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of ELF::Extract::Sections
