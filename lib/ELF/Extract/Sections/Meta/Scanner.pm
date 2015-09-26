399a166e2878efd7fd9a3e9161f6995cf20bd498
Kent Fredric <kentfredric@gmail.com>
2015-09-26 18:21:44 +1200

    Build results of b315e8b (on master)

--- lib/ELF/Extract/Sections/Meta/Scanner.pm
+++ lib/ELF/Extract/Sections/Meta/Scanner.pm
@@ -6,7 +6,7 @@ package ELF::Extract::Sections::Meta::Scanner;
 
 # ABSTRACT: Interface Contract for Scanners
 
-our $VERSION = '1.001001';
+our $VERSION = '1.001001'; # TRIAL
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
